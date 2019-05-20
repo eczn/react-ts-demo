3b84eeebaa10a2b3436802045ca4087daff075e6
matteokjh <429797371@qq.com>
2019-05-20 21:32:10 +0800

    '[A]test'

---
+++ src/helloworld/helloworld.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './helloworld.css';
+
+export default class hello extends React.Component {
+    render(){
+        return (
+            <h1 className='header'>
+                welcome!This is a Index page.
+            </h1>
+        )
+    }
+}
